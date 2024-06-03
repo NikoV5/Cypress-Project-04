ad6e9c927dd4f85e6df9c4a6f15bb6db0a02f2d8
NikoV5 <NikoVourtsis7@gmail.com>
2024-06-03 17:18:03 -0500

    Added updated files

---
+++ cypress/e2e/integration/project-04.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress"/>
+
+
+
+describe('Project 4 - ', () => {
+  beforeEach(() => {
+    cy.visit('https://www.techglobal-training.com/frontend/project-6');
+  
+})
+
+  
+it('Test Case 1 - Validate the default Book your trip form', () => {
+ 
+
+})
+
+});
